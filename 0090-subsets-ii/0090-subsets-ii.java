279422c90ed39feb39652f537dbcd4cece42a0ad
LANKA NAGAMOHAN <2400030532@kluniversity.in>
2026-08-12 15:12:37 +0530

    Time: 2 ms (99.72%), Space: 45.1 MB (60.44%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer>l=new ArrayList<>();
+        List<List<Integer>>ans=new ArrayList<>();
+        int n=nums.length;
+        Arrays.sort(nums);
+        backtrack(nums,0,n,new ArrayList<>(),ans);
+        return ans;
+    }
+    private static void backtrack(int[] a,int s,int n,List<Integer>l,List<List<Integer>>ans){
+      
+        ans.add(new ArrayList<>(l));
+        
+        for(int i=s;i<n;i++){
+       //choose
+         if(i>s && a[i-1]==a[i]){
+            continue;
+         }
+        l.add(a[i]);
+       //explore
+        backtrack(a,i+1,n,l,ans);
+       //undo
+       l.remove(l.size()-1);
+        }
+
+    }
+}
