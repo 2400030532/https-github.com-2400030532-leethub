3a866b6d6076ee051cd8350045a2581682eb5c54
LANKA NAGAMOHAN <2400030532@kluniversity.in>
2026-08-24 19:31:47 +0530

    Time: 2 ms (60.49%), Space: 43.7 MB (83.57%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+       
+        if(k==0){
+            for(int i=0;i<n;i++){
+                code[i]=0;
+            }
+            return code;
+        }
+        int[] ans=new int[n];
+           for(int i=0;i<n;i++){
+             int sum=0;
+             if(k>0){
+            for(int j=1;j<=k;j++){
+                sum+=code[(i+j)%n];
+            }
+           
+           }else{
+                for(int j=1;j<=-k;j++){
+                    sum+=code[((i-j)+n)%n];
+                }
+           }
+           ans[i]=sum;
+    }
+       
+        return ans;
+    }
+}
